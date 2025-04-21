d2af05f4423c9ffe5c89e85890dbfca84a51408a
parvez-tm <parvez.tm@outlook.com>
2025-04-21 16:34:19 +0530

    work in progress

---
+++ src/decorator/method.decorator.ts
@@ -0,0 +1,10 @@
+function LogMethod(target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
+    const originalMethod = descriptor.value;
+  
+    descriptor.value = function(...args: any[]) {
+      console.log(`Method ${propertyKey} called with arguments:`, args);
+      const result = originalMethod.apply(this, args);
+      console.log(`Method ${propertyKey} returned:`, result);
+      return result;
+    };
+  }
